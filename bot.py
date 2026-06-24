952b7760e951d5a0174dd233f27949fe9712e57d
moroomar804-cyber <moroomar804@gmail.com>
2026-06-24 05:11:48 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from telebot import types
 import requests
 
 # هذا هو التوكن الخاص بك كما طلبته
-BOT_TOKEN = "8837023232:AAGs1dUoRw9U_EuCJx2ElIP3MoAB0qhDLTs"
+BOT_TOKEN = "8837023232:AAFloK7HfJ-IqPsH5ZHNXhItGZxhEZjDxVE"
 SIM_API_KEY = "EyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE4MTM3OTkwMTMsImlhdCI6MTc4MjI2MzAxMywicmF5IjoiMjc3N2QwNzI5YmUyY2E3Yzg4N2Y3MDI1NzA0MzRmY2UiLCJzdWIiOjQyNDcyOTB9.w7F7enP2N6lz0Qv2s_QUYZfz_PzkB8Yh0ju_wPB1QTox9vM3bsDpB6qt2YjwReBER-7jGVp7_bvvoJPUCD5jA7UxBYvUNZ1M0El2w8gjtcdjW4eajsPVsW6aiMeif2QABPk7NMnDRgPOtSqEtQQdD1u-MxoGq-e6e9o3fe7lZV-zWR7WinStnRxOkbtkuJPI4Wrrd378b0YwIdyxd6W_uJgvf67GoNi0IWqXUJt3zGrnVec7UmJOP-eIG4Bpr4VVrAQLOTcFRjGdntbFxIBCKyo1IJw6W-FWZD54mJNUoidPHh-eDN7ZC42BUeKaRfxCgSiBuVkVipP2UvD9MENh9Q"
 SUPPORT_USER = "@Smsteleteam"
 
@@ -33,4 +33,3 @@ def handle_menu(message):
         bot.send_message(message.chat.id, f"تواصل مع: {SUPPORT_USER}")
 
 if __name__ == "__main__":
-    bot.polling(none_stop=True)
